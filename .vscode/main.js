39014763e6c7fa71b45d5f2cc3328b84926dd0d7
tharanga wijesinghe <tharangawijesinghe@gmail.com>
2024-08-12 23:31:02 -0500

    first commit

---
+++ .vscode/main.js
@@ -0,0 +1,7 @@
+var typed = new typed(".text", {
+    strings: ["Full Stack Web Developer", "Front End Developer", "Web Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
